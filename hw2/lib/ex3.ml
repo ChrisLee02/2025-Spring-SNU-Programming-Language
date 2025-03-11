8998e0a2df9b84f4d84f4927897aaba7d83529de
Chrislee02 <hdchris765@snu.ac.kr>
2025-03-12 02:12:15 +0900

    hw2 done

--- hw2/lib/ex3.ml
+++ hw2/lib/ex3.ml
@@ -13,8 +13,27 @@ let goLeft loc =
   | LOC (_, HAND ([], _, _)) ->
       raise (NOMOVE "left of first")
 
-(* 
-goRight: location -> location
-goUp: location -> location
-goDown: location -> location
-를 정의하세용 *)
+let goRight loc =
+  match loc with
+  | LOC (_, TOP) -> raise (NOMOVE "right of top")
+  | LOC (t, HAND (left, up, r :: right)) ->
+      LOC (r, HAND (t :: left, up, right))
+  | LOC (_, HAND (_, _, [])) ->
+      raise (NOMOVE "right of last")
+
+let goUp loc =
+  match loc with
+  | LOC (_, TOP) -> raise (NOMOVE "up of top")
+  | LOC (t, HAND (left, up, right)) ->
+      LOC (NODE (List.rev left @ [ t ] @ right), up)
+
+let goDown loc =
+  (* go to leftist of child nodes *)
+  match loc with
+  | LOC (LEAF _, _) -> raise (NOMOVE "down of Leaf")
+  | LOC (NODE [], _) ->
+      raise (NOMOVE "down of empty children list")
+  | LOC (NODE (first :: child_list), zipper) ->
+      let up = zipper in
+      let zipper = HAND ([], up, child_list) in
+      LOC (first, zipper)
